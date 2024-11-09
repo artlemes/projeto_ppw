5884e7d82c2998384d499fef4a25eb26cc228ea8
Renan Silva <renansantos.silva04@outlook.com>
2024-11-09 01:07:06 -0300

    First commit back

---
+++ back/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+
+})
