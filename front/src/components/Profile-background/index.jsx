1fad245323eabff51f1e1d2078c48614f52a91f5
artlemes <arthur.c.lemes@gmail.com>
2024-12-03 15:29:37 -0300

    fundo do perfil feito

---
+++ front/src/components/Profile-background/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Profile-background.module.css"
+
+function ProfileBackground() {
+    return (
+        <header className={styles.profile}></header>
+    )
+}
+
+export default ProfileBackground
--- front/src/pages/Perfil/index.jsx
+++ front/src/pages/Perfil/index.jsx
@@ -1,9 +1,15 @@
 import Sider from "../../components/Sider/index.jsx"
+import ProfileBackground from "../../components/Profile-background/index.jsx";
+import styles from "./Perfil.module.css"
 
 
 function Perfil() {
     return (
+        <div className={styles.fundoPagina}>
         <Sider></Sider>
+        <ProfileBackground></ProfileBackground>
+        <Sider></Sider>
+        </div>
     )
 
 }
