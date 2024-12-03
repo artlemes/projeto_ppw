e6516720c9fed32e816ea517658a2ca3f1dea035
artlemes <arthur.c.lemes@gmail.com>
2024-12-03 15:43:38 -0300

    sider para foto do perfil

--- front/src/components/Profile-background/index.jsx
+++ front/src/components/Profile-background/index.jsx
@@ -1,8 +1,14 @@
 import styles from "./Profile-background.module.css"
+import ProfileSider from "../ProfileSider"
 
 function ProfileBackground() {
     return (
+        <>
+        <ProfileSider></ProfileSider>
         <header className={styles.profile}></header>
+        
+        </>
+
     )
 }
 
---
+++ front/src/components/ProfileSider/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./ProfileSider.module.css"
+
+function ProfileSider() {
+    return (
+        <header className={styles.profileSider}></header>
+        
+    )
+}
+
+export default ProfileSider
