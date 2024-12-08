aa7a4a8fc991e5cc21268ed746a9389ed4dd277a
artlemes <arthur.c.lemes@gmail.com>
2024-12-08 20:04:42 -0300

    tela de listagem de anuncios pronta
    ela ta pronta mas nao puxa categorias

--- front/src/routes.js
+++ front/src/routes.js
@@ -9,6 +9,7 @@ import Anuncio from "./pages/Anuncio";
 import Padrao from "./pages/Padrao";
 import RotaProtegida from "./utils/RotaProtegida";
 import CriarAnuncio from "./pages/CriarAnuncio"; // Importa o componente
+import ListagemDeAnuncios from "./pages/ListagemDeAnuncios";
 
 function AppRoutes() {
     return (
@@ -19,7 +20,8 @@ function AppRoutes() {
                 <Route path="/cadastro" element={<Cadastro/>}/>
                 <Route path="/recuperarSenha" element={<RecuperarSenha/>}/>
                 <Route path="*" element={<PagNaoEncontrada/>}/>
-
+                <Route path="/listagem" element={<ListagemDeAnuncios/>}/>
+                
                 {/* aqui protege a rota /perfil com o componente HOC criado em utils, sempre que quiser proteger envelopa aassim*/}
                 <Route path="/perfil" element={<RotaProtegida><Padrao><Perfil/></Padrao></RotaProtegida>}/>
                 <Route path="/anuncio/buscar" element={<RotaProtegida><Padrao><Anuncio/></Padrao></RotaProtegida>}/>
